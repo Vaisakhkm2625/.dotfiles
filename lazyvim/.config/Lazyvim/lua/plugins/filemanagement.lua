7d118157e2a0226eb22b7dbc2615c3c659a79fbc
Vaisakh K M <vaisakhkm2625@gmail.com>
2023-12-18 03:16:40 +0530

    added oil to nvim

--- lazyvim/.config/Lazyvim/lua/plugins/filemanagement.lua
+++ lazyvim/.config/Lazyvim/lua/plugins/filemanagement.lua
@@ -1,6 +1,13 @@
 return {
-  {
-    "ThePrimeagen/harpoon",
+    {
+        'stevearc/oil.nvim',
+        opts = {},
+        -- Optional dependencies
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+
+    {
+        "ThePrimeagen/harpoon",
         opts = {
             global_settings = {
                 tabline = false,
@@ -11,75 +18,75 @@ return {
                 width = vim.api.nvim_win_get_width(0) - 10,
             },
         },
-    keys = {
-      {
-        "<leader>m",
-        function()
-          require("harpoon.mark").add_file()
-        end,
-        desc = "add mark to harpoon",
-      },
-      {
-        "<leader>hu",
-        function()
-          require("harpoon.ui").toggle_quick_menu()
-        end,
-        desc = "toggle harpoon ui",
-      },
-      {
-        "<leader>1",
-        function()
-          require("harpoon.ui").nav_file(1)
-        end,
-        desc = "harpoon file 1",
-      },
-      {
-        "<leader>2",
-        function()
-          require("harpoon.ui").nav_file(2)
-        end,
-        desc = "harpoon file 2",
-      },
-      {
-        "<leader>3",
-        function()
-          require("harpoon.ui").nav_file(3)
-        end,
-        desc = "harpoon file 3",
-      },
-      {
-        "<leader>4",
-        function()
-          require("harpoon.ui").nav_file(4)
-        end,
-        desc = "harpoon file 4",
-      },
-      {
-        "<leader>hc",
-        function()
-          require("harpoon.cmd-ui").toggle_quick_menu()
-        end,
-        desc = "harpoon command ui",
-      },
-      {
-        "<leader>h1",
-        function()
-          require("harpoon.tmux").sendCommand(1, 1)
-        end,
-        desc = "harpoon sent command to 1 buffer",
-      },
-      {
-        "<leader>h2",
-        function()
-          require("harpoon.tmux").sendCommand(1, 1)
-        end,
-        desc = "harpoon sent command to 2 buffer",
-      },
+        keys = {
+            {
+                "<leader>m",
+                function()
+                    require("harpoon.mark").add_file()
+                end,
+                desc = "add mark to harpoon",
+            },
+            {
+                "<leader>hu",
+                function()
+                    require("harpoon.ui").toggle_quick_menu()
+                end,
+                desc = "toggle harpoon ui",
+            },
+            {
+                "<leader>1",
+                function()
+                    require("harpoon.ui").nav_file(1)
+                end,
+                desc = "harpoon file 1",
+            },
+            {
+                "<leader>2",
+                function()
+                    require("harpoon.ui").nav_file(2)
+                end,
+                desc = "harpoon file 2",
+            },
+            {
+                "<leader>3",
+                function()
+                    require("harpoon.ui").nav_file(3)
+                end,
+                desc = "harpoon file 3",
+            },
+            {
+                "<leader>4",
+                function()
+                    require("harpoon.ui").nav_file(4)
+                end,
+                desc = "harpoon file 4",
+            },
+            {
+                "<leader>hc",
+                function()
+                    require("harpoon.cmd-ui").toggle_quick_menu()
+                end,
+                desc = "harpoon command ui",
+            },
+            {
+                "<leader>h1",
+                function()
+                    require("harpoon.tmux").sendCommand(1, 1)
+                end,
+                desc = "harpoon sent command to 1 buffer",
+            },
+            {
+                "<leader>h2",
+                function()
+                    require("harpoon.tmux").sendCommand(1, 1)
+                end,
+                desc = "harpoon sent command to 2 buffer",
+            },
+        },
+        --      lazy = false,
+
+
     },
-  --      lazy = false,
-
-
-  },
 }
 
 --[[
