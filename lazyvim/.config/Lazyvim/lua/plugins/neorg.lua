161fd4b0d4f3df18b3992716c367cd158150f6d3
Vaisakh K M <vaisakhkm2625@gmail.com>
2023-12-18 03:17:27 +0530

    neorg

--- lazyvim/.config/Lazyvim/lua/plugins/neorg.lua
+++ lazyvim/.config/Lazyvim/lua/plugins/neorg.lua
@@ -11,7 +11,10 @@ return {
     opts = {
       load = {
         ["core.defaults"] = {}, -- Loads default behaviour
-        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.integrations.image"] = {},
+        ["core.concealer"] = { -- Adds pretty icons to your documents
+                    icon_preset = "diamond"
+                }, 
         ["core.summary"] = {},
         ["core.dirman"] = { -- Manages Neorg workspaces
           config = {
